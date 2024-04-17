01a212c34ee8b5330b9929708dd94f3440d62d54
githubscientist <sathish.datascientist@gmail.com>
2024-04-17 20:49:30 +0530

    event handling --forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,42 @@
-const App = () => {
+import Note from "./components/Note";
 
-  const handleClick = () => console.log('Increment button is clicked')
+const App = ({ notes }) => {
+
+  const addNote = (e) => {
+    // prevent the default action of submitting the form
+    e.preventDefault();
+
+    // get the value of the input field
+    const content = e.target.elements[0].value;
+
+    // create a new note object with the content and a unique id
+    const noteObject = {
+      content: content,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
+    }
+
+    // add the note object to the array of notes
+    notes.push(noteObject);
+
+    console.log(notes);
+  }
 
   return (
     <div>
-      <button onClick={handleClick}>Increment</button>
+      <h1>Notes</h1>
+      <ul>
+        {
+          notes.map(note => 
+            <Note note={note} key={ note.id } />
+          )
+        }
+      </ul>
+      <form onSubmit={addNote}>
+        <input type="text" />
+        <button type="submit">save</button>
+      </form>
     </div>
   )
 }
