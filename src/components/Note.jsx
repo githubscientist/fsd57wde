26cb40a955ab059ed56a347d163948073a7ca1e6
githubscientist <sathish.datascientist@gmail.com>
2024-04-17 20:00:39 +0530

    conditional rendering --using ternary operator

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,8 +1,9 @@
 const Note = ({ note }) => {
-    if (note.important) {
-        return <li>{note.content} { '⭑' }</li>;
-    }
-    return <li>{note.content} { '⭒' }</li>;
+    return <li>
+        {
+            note.important ? note.content + ' ⭑' : note.content + ' ⭒'
+        }
+    </li>
 }
 
 export default Note;
