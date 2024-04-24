0f60d94f95f810e5d2a74444040eb54498567b81
githubscientist <sathish.datascientist@gmail.com>
2024-04-24 19:36:39 +0530

    root route --login, register as separate routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./components/Home";
+import Register from "./components/Register";
+import Login from "./components/Login";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <h1>Hello, World!</h1>
+    element: <Home />
+  },
+  {
+    path: "/register",
+    element: <Register />
+  },
+  {
+    path: "/login",
+    element: <Login />
   }
 ])
 
