92e8b815542b6142fe1008d5f01da6da6c182979
githubscientist <sathish.datascientist@gmail.com>
2024-04-24 18:19:34 +0530

    cleaned up

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,7 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Home from "./components/Home";
-import Login from "./components/Login";
-import Register from "./components/Register";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-    children: [
-      {
-        path: "login",
-        element: <Login />,
-        children: [
-          {
-            path: "forgot-password",
-            element: <div>Forgot Password</div>
-          }
-        ]
-      },
-      {
-        path: "register",
-        element: <Register />
-      }
-    ]
-  },
-])
-
 const App = () => {
-  return <RouterProvider router={router} />
+  return (
+    <div>App</div>
+  )
 }
 
 export default App;
