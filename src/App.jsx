655251efe3e83c68e84103f8c1640a42e25d555c
githubscientist <sathish.datascientist@gmail.com>
2024-04-26 19:28:21 +0530

    task

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,38 @@
+import { useState } from "react";
+import Card from "./Card";
+
+const products = [
+    {
+        id: 1,
+        name: 'Product 1',
+        price: 100
+    },
+    {
+        id: 2,
+        name: 'Product 2',
+        price: 200
+    },
+    {
+        id: 3,
+        name: 'Product 3',
+        price: 300
+    }
+]
+
 const App = () => {
+
+    const [cart, setCart] = useState([]);
+
   return (
-    <h1>Hello, World!</h1>
+    <div className="container">
+          <div className="row">
+              {
+              products.map(product => (
+                  <Card key={product.id} product={product} cart={cart} setCart={setCart} />
+              ))
+        }
+          </div>
+    </div>
   )
 }
 
