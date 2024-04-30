3b5c07242f7fc3ee92d7e6d8b88ff134850cc5e7
githubscientist <sathish.datascientist@gmail.com>
2024-04-30 19:20:22 +0530

    reading data from json file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-import { useState } from 'react';
-import products from './assets/product.json';
+import { useEffect, useState } from 'react';
+// import products from './assets/product.json';
 
 const App = () => {
 
-  const [cart, setCart] = useState(products);
+  const [cart, setCart] = useState();
 
-  console.log(cart);
+  // const fetchData = async () => {
+  //   const response = await fetch('/src/assets/product.json');
+  //   const data = await response.json();
+  //   console.log(data);
+  // }
+
+  useEffect(() => {  
+    // fetchData();
+    fetch('/src/assets/product.json')
+      .then(res => res.json())
+      .then(data => setCart(data));
+  }, []);
+
+  useEffect(() => {
+    console.log(cart);
+  }, [cart]);
 
   return (
     <div>App</div>
