6195752eb62ecb5e47e34b9b77f3bdeb296fec20
githubscientist <sathish.datascientist@gmail.com>
2024-04-30 19:48:47 +0530

    board creation done -- state for handling squares added

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,40 @@
 import { useEffect, useState } from 'react';
-// import products from './assets/product.json';
+import './App.css';
 
 const App = () => {
 
-  const [cart, setCart] = useState();
+  const [squares, setSquares] = useState(Array(9).fill(null));
 
-  // const fetchData = async () => {
-  //   const response = await fetch('/src/assets/product.json');
-  //   const data = await response.json();
-  //   console.log(data);
-  // }
-
-  useEffect(() => {  
-    // fetchData();
-    fetch('/src/assets/product.json')
-      .then(res => res.json())
-      .then(data => setCart(data));
-  }, []);
+  const handleClick = (i) => {
+    const newSquares = squares.slice();
+    newSquares[i] = 'X';
+    setSquares(newSquares);
+  }
 
   useEffect(() => {
-    console.log(cart);
-  }, [cart]);
+    console.log(squares);
+  }, [squares]);
 
   return (
-    <div>App</div>
+    <div>
+      <div className='row'>
+        <button className='square' onClick={() => handleClick(0)}></button>
+        <button className='square' onClick={() => handleClick(1)}></button>
+        <button className='square' onClick={() => handleClick(2)}></button>
+      </div>
+
+      <div className='row'>
+        <button className='square' onClick={() => handleClick(3)}></button>
+        <button className='square' onClick={() => handleClick(4)}></button>
+        <button className='square' onClick={() => handleClick(5)}></button>
+      </div>
+
+      <div className='row'>
+        <button className='square' onClick={() => handleClick(6)}></button>
+        <button className='square' onClick={() => handleClick(7)}></button>
+        <button className='square' onClick={() => handleClick(8)}></button>
+      </div>
+    </div>
   )
 }
 
