5ef4938403c66486df3f4f713e6084052ec1ac81
githubscientist <sathish.datascientist@gmail.com>
2024-05-03 19:53:30 +0530

    update completed --crud

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,6 +1,9 @@
 import { Link, useLoaderData } from "react-router-dom";
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEdit } from "@fortawesome/free-regular-svg-icons";
+import { set } from "firebase/database";
 
 // make the data available to the component
 export async function loader() {
@@ -13,14 +16,18 @@ const Todos = () => {
     const { todos } = useLoaderData();
     const [title, setTitle] = useState('');
     const [description, setDescription] = useState('');
+    const [completed, setCompleted] = useState(false);
+    const [isEditing, setIsEditing] = useState(false);
+    const [todo, setTodo] = useState(null);
 
-    const handleSubmit = (e) => {
+    const handleAddClick = (e) => {
         e.preventDefault();
 
         // create a new todo object
         const newTodo = {
             title,
-            description
+            description,
+            completed: completed === 'completed' ? true : false
         }
 
         // add the new todo to the list of todos
@@ -40,18 +47,66 @@ const Todos = () => {
         setDescription('');
     }
 
+    const handleEdit = (todo) => {
+        setIsEditing(true);
+        setTitle(todo.title);
+        setDescription(todo.description);
+        setCompleted(todo.completed);
+        setTodo(todo);
+    }
+
+    const handleUpdateClick = (e) => {
+        e.preventDefault();
+        
+        const updatedTodo = {
+            title,
+            description,
+            completed: completed === 'completed' ? true : false
+        }
+
+        axios.put(`https://66339eeef7d50bbd9b4a170f.mockapi.io/todos/${todo.id}`, updatedTodo)
+            .then((response) => {
+                alert('Todo updated successfully!');
+                setIsEditing(false);
+                window.location.reload();
+            })
+            .catch((error) => {
+                alert('Failed to update todo!');
+            });
+    }
+
   return (
       <div>
-            <h1>Todos</h1>
-            <ul>
+          <h1>Todos</h1>
+            <ul style={{ listStyleType: 'none', fontSize: '18px' }}>
                 {todos.map((todo) => (
-                    <li key={todo.id}>
-                        <Link to={`/todos/${todo.id}`}><p>{todo.title}</p></Link>
+                    <li
+                        key={todo.id}
+                        style={{ textDecoration: todo.completed ? 'line-through' : 'none', marginBottom: '20px'}}
+                    >
+                        <input 
+                            type="checkbox"
+                            checked={todo.completed}
+                            onChange={(e) => {
+                                
+                            }}
+                        />
+                        <Link
+                            to={`/todos/${todo.id}`}
+                            style={{
+                                marginRight: '20px', textDecoration: 'none', color: todo.completed ? 'gray' : 'black'
+                            }}
+                        >{todo.title}</Link>
+                        <FontAwesomeIcon
+                            icon={faEdit} 
+                            onClick={() => handleEdit(todo)}
+                            style={{ cursor: 'pointer' }}
+                        />
                     </li>
                 ))}
           </ul>
           
-          <form onSubmit={handleSubmit}>
+          <form>
             <input
                 type="text"
                 placeholder="Enter todo title..." 
@@ -63,8 +118,22 @@ const Todos = () => {
                 placeholder="Enter todo description..." 
                 value={description}
                 onChange={(e) => setDescription(e.target.value)}    
-            />
-            <button type="submit">Add Todo</button>
+              />
+              <select
+                value={completed ? 'completed' : 'not completed'}
+                onChange={(e) => setCompleted(e.target.value)}
+              >
+                <option>not completed</option>
+                <option>completed</option>
+            </select>
+              {
+                  isEditing ? (
+                  <button
+                    onClick={handleUpdateClick}
+                  >Update Todo</button>
+              ) : (
+                <button onClick={handleAddClick}>Add Todo</button>
+            )}
           </form>
     </div>
   )
